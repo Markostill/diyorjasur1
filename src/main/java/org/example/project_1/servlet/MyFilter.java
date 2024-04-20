bdc82174a9de6b7422649955e6de0e40dbcfbb16
getStuden <hayperpc.7@gmail.com>
2024-04-20 09:44:48 +0500

    MyFilter

--- src/main/java/org/example/project_1/servlet/MyFilter.java
+++ src/main/java/org/example/project_1/servlet/MyFilter.java
@@ -2,14 +2,10 @@ package org.example.project_1.servlet;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Optional;
 
 @WebFilter(urlPatterns = "/user/*")
 public class MyFilter implements Filter {
@@ -23,7 +19,7 @@ public class MyFilter implements Filter {
         if(obj == null){
             res.sendRedirect("http://localhost:8080/login/Login.jsp");
         } else {
-            res.sendRedirect("http://localhost:8080/index.jsp");
+            chain.doFilter(request, response);
         }
     }
 
